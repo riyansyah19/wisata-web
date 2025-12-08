173f2da6cfdef9aa39f4f500f744a5c74f27f26d
mahruss203 <mahrzstwn@gmail.com>
2025-12-09 02:26:38 +0700

    UP-24-migration-database

---
+++ database/seeders/DummyUsersSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DummyUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userData = [
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('12345678'),
+            ],
+            ];
+
+            foreach($userData as $key => $val){
+                User::create($val);
+            }
+    }
+}
